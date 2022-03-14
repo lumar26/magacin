4782c42c59ed08cd51ea77256a0e4a6791bd5b76
Luka Marinković <lukamarinkovic2602@gmail.com>
2022-03-14 10:14:06 +0100

    Implementiran interfejs IMagacin u klasi Magacin

--- src/interfejs/IMagacin.java
+++ src/interfejs/IMagacin.java
@@ -4,6 +4,6 @@ import artikal.Artikal;
 
 public interface IMagacin {
     void add(Artikal artikal);
-    void remove(Artikal artikal);
+    void remove(Artikal artikal) throws Exception;
     Artikal find(int sifra);
 }
--- src/magacin/Magacin.java
+++ src/magacin/Magacin.java
@@ -3,20 +3,35 @@ package magacin;
 import artikal.Artikal;
 import interfejs.IMagacin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Magacin implements IMagacin {
+
+    private final Map<Integer, Artikal> artikli = new HashMap();
 
     @Override
     public void add(Artikal artikal) {
-
+        if (artikli.containsKey(artikal.getSifra())) {
+            Artikal temp = artikli.get(artikal.getSifra());
+            temp.setKolicina(temp.getKolicina() + artikal.getKolicina());
+        } else {
+            artikli.put(artikal.getSifra(), artikal);
+        }
     }
 
     @Override
-    public void remove(Artikal artikal) {
-
+    public void remove(Artikal artikal) throws Exception {
+        if (!artikli.containsKey(artikal.getSifra())) {
+            Artikal temp = artikli.get(artikal.getSifra());
+            temp.setKolicina(temp.getKolicina() + artikal.getKolicina());
+            throw new Exception("Ne postoji artikal za uklanjanje");
+        }
+        artikli.get(artikal.getSifra()).setKolicina(artikli.get(artikal.getSifra()).getKolicina() - artikal.getKolicina());
     }
 
     @Override
     public Artikal find(int sifra) {
-        return null;
+        return artikli.get(sifra);
     }
 }
